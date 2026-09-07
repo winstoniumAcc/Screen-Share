93feb722c2636add1fe862445488937848a6dbd7
“winstoniumAcc” <winstonarcher2010@gmail.com>
2026-09-07 11:33:44 +0700

    Update screen monitoring

--- server.js
+++ server.js
@@ -8,72 +8,80 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-const participants = new Map();
+app.get("/participant", (req, res) => {
+    res.sendFile(__dirname + "/public/participant.html");
+});
+
+app.get("/admin", (req, res) => {
+    res.sendFile(__dirname + "/public/admin.html");
+});
 
 io.on("connection", (socket) => {
+
     console.log("Connected:", socket.id);
 
     socket.on("join-participant", (name) => {
-        participants.set(socket.id, {
-            name: name || "Unknown",
-            socketId: socket.id
-        });
 
-        io.emit("participant-joined", {
+        socket.data.name = name;
+
+        socket.broadcast.emit("participant-joined", {
             socketId: socket.id,
-            name: name || "Unknown"
+            name: name
         });
 
-        socket.emit(
-            "participant-list",
-            Array.from(participants.values())
+        socket.emit("participant-list",
+            [...io.sockets.sockets.values()]
+                .filter(s => s.data.name)
+                .map(s => ({
+                    socketId: s.id,
+                    name: s.data.name
+                }))
         );
     });
 
-    socket.on("join-admin", () => {
-        socket.emit(
-            "participant-list",
-            Array.from(participants.values())
-        );
-    });
-
-    socket.on("offer", ({ target, offer }) => {
-        io.to(target).emit("offer", {
+    socket.on("offer", data => {
+        io.to(data.target).emit("offer", {
             sender: socket.id,
-            offer
+            offer: data.offer
         });
     });
 
-    socket.on("answer", ({ target, answer }) => {
-        io.to(target).emit("answer", {
+    socket.on("answer", data => {
+        io.to(data.target).emit("answer", {
             sender: socket.id,
-            answer
+            answer: data.answer
         });
     });
 
-    socket.on("ice-candidate", ({ target, candidate }) => {
-        io.to(target).emit("ice-candidate", {
+    socket.on("ice-candidate", data => {
+        io.to(data.target).emit("ice-candidate", {
             sender: socket.id,
-            candidate
+            candidate: data.candidate
         });
     });
 
     socket.on("disconnect", () => {
-        if (participants.has(socket.id)) {
-            const participant = participants.get(socket.id);
 
-            participants.delete(socket.id);
+        if (socket.data.name) {
 
             io.emit("participant-left", {
                 socketId: socket.id,
-                name: participant.name
+                name: socket.data.name
             });
 
-            console.log("Left:", participant.name);
+            console.log(
+                `${socket.data.name} disconnected`
+            );
         }
     });
+
 });
 
-server.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+
+// IMPORTANT:
+// 0.0.0.0 allows connections from outside the server.
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
