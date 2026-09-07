6b855058c3a684e81f0e83b6435660f619aebfa8
“winstoniumAcc” <winstonarcher2010@gmail.com>
2026-09-07 12:24:25 +0700

    Fix screen sharing system

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+app.get("/participant", (req, res) => {
+    res.sendFile(__dirname + "/public/participant.html");
+});
+
+app.get("/admin", (req, res) => {
+    res.sendFile(__dirname + "/public/admin.html");
+});
+
 io.on("connection", (socket) => {
 
     console.log("Connected:", socket.id);
